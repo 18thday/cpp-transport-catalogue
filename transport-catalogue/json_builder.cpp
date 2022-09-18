c33afc2a13d64b920911b46e0e630eaa6110e913
18thday <day18th@yandex.ru>
2022-09-18 13:55:57 +0500

    after review

--- transport-catalogue/json_builder.cpp
+++ transport-catalogue/json_builder.cpp
@@ -1,157 +1,152 @@
 #include "json_builder.h"
 
+#include <utility>
+
 using namespace std;
 
 namespace json{
 
 KeyContext Builder::Key(string key){
-    //cerr << "Key() : begin "sv << endl;
-    if (nodes_stack_.empty() || need_be_build){
-        throw std::logic_error("error Key(string key) : no opened Nodes"s);
-    }
-    if (!nodes_stack_.back()->IsDict()){
-        throw std::logic_error("error Key(string key) : the last opened Node is not json::Dict"s);
-    }
-    Dict& tmp = get<Dict>(nodes_stack_.back()->GetValue());
-    nodes_stack_.emplace_back(&tmp[move(key)]);
-    //cerr << "\t - end"sv << endl;
-    return *this;
+	//cerr << "Key() : begin "sv << endl;
+	if (nodes_stack_.empty() || expects_build_method){
+		throw std::logic_error("error Key(string key) : no opened Nodes"s);
+	}
+	if (!nodes_stack_.back()->IsDict()){
+		throw std::logic_error("error Key(string key) : the last opened Node is not json::Dict"s);
+	}
+	Dict& tmp = get<Dict>(nodes_stack_.back()->GetValue());
+	nodes_stack_.emplace_back(&tmp[move(key)]);
+	//cerr << "\t - end"sv << endl;
+	return *this;
 }
 
-BuilderContext Builder::Value(Node::Value value){
-    if (root_.IsBool() || root_.IsDouble() || root_.IsString() || need_be_build){
-        throw std::logic_error("Need be Build() after Value()"s);
+Builder& Builder::Value(Node::Value value){
+    if (root_.IsBool() || root_.IsDouble() || root_.IsString() || expects_build_method){
+    	throw std::logic_error("Need be Build() after Value()"s);
     }
-    //cerr << "Value() : begin "sv << endl;
-    if (nodes_stack_.empty() && root_.IsNull()){
-        need_be_build = true;
-        root_.GetValue() = move(value);
-    } else if (nodes_stack_.back()->IsNull()) {
-        nodes_stack_.back()->GetValue() = move(value);
-        nodes_stack_.pop_back();
-    } else if (nodes_stack_.back()->IsArray()) {
-        get<Array>(nodes_stack_.back()->GetValue()).emplace_back(move(value));
-    } else {
-        throw std::logic_error("error Value(Node::Value value) : the last opened Node is not Null"s);
-    }
-    //cerr << "\t - end"sv << endl;
-    this->empty_json_builder = false;
-    return (*this);
+	//cerr << "Value() : begin "sv << endl;
+	if (nodes_stack_.empty() && root_.IsNull()){
+		expects_build_method = true;
+		root_.GetValue() = move(value);
+	} else if (nodes_stack_.back()->IsNull()) {
+		nodes_stack_.back()->GetValue() = move(value);
+		nodes_stack_.pop_back();
+	} else if (nodes_stack_.back()->IsArray()) {
+		get<Array>(nodes_stack_.back()->GetValue()).emplace_back(move(value));
+	} else {
+		throw std::logic_error("error Value(Node::Value value) : the last opened Node is not Null"s);
+	}
+	//cerr << "\t - end"sv << endl;
+	this->empty_json_builder = false;
+	return (*this);
 }
 
 DictContext Builder::StartDict(){
-    if (need_be_build){
-        throw std::logic_error("Need be Build() after Value()"s);
-    }
-    //cerr << "StartDict() : begin "sv << endl;
-    if (nodes_stack_.empty()){
-        root_ = (Dict {});
-        nodes_stack_.emplace_back(&root_);
-    } else if (nodes_stack_.back()->IsNull() ){
-        nodes_stack_.back()->GetValue() = Dict{};
-    } else if (nodes_stack_.back()->IsArray()){
-        cerr << " ADD TO ARRAY ";
+	if (expects_build_method){
+		throw std::logic_error("Need be Build() after Value()"s);
+	}
+	//cerr << "StartDict() : begin "sv << endl;
+	if (nodes_stack_.empty()){
+		root_ = (Dict {});
+		nodes_stack_.emplace_back(&root_);
+	} else if (nodes_stack_.back()->IsNull() ){
+		nodes_stack_.back()->GetValue() = Dict{};
+	} else if (nodes_stack_.back()->IsArray()){
+		cerr << " ADD TO ARRAY ";
 
-        get<Array>(nodes_stack_.back()->GetValue()).emplace_back(move(Dict {}));
-        nodes_stack_.emplace_back(&get<Array>(nodes_stack_.back()->GetValue()).back());
+		get<Array>(nodes_stack_.back()->GetValue()).emplace_back(move(Dict {}));
+		nodes_stack_.emplace_back(&get<Array>(nodes_stack_.back()->GetValue()).back());
 
-    } else {
-        throw std::logic_error("error StartDict() : the last opened Node is not Null or Array"s);
-    }
-    //cerr << "\t- end"sv << endl;
-    this->empty_json_builder = false;
-    return *this;
+	} else {
+		throw std::logic_error("error StartDict() : the last opened Node is not Null or Array"s);
+	}
+	//cerr << "\t- end"sv << endl;
+	this->empty_json_builder = false;
+	return *this;
 }
 
-CommonContext Builder::EndDict(){
-    if (need_be_build){
-        throw std::logic_error("Need be Build() after Value()"s);
-    }
-    //cerr << "EndDict() : begin "sv << endl;
-    if(!nodes_stack_.back()->IsDict()){
-        throw std::logic_error("error EndDict() : the last opened Node is not json::Dict"s);
-    }
-    nodes_stack_.pop_back();
-    //cerr << "\t- end"sv << endl;
-    return *this;
+Builder& Builder::EndDict(){
+	if (expects_build_method){
+		throw std::logic_error("Need be Build() after Value()"s);
+	}
+	//cerr << "EndDict() : begin "sv << endl;
+	if(!nodes_stack_.back()->IsDict()){
+		throw std::logic_error("error EndDict() : the last opened Node is not json::Dict"s);
+	}
+	nodes_stack_.pop_back();
+	//cerr << "\t- end"sv << endl;
+	return *this;
 }
 
 ArrayContext Builder::StartArray(){
-    if (need_be_build){
-        throw std::logic_error("Need be Build() after Value()"s);
-    }
-    this->empty_json_builder = false;
-    //cerr << "StartArray() : begin "sv << endl;
-    if (nodes_stack_.empty()){
-        root_ = (Array {});
-        nodes_stack_.emplace_back(&root_);
-    } else if (nodes_stack_.back()->IsNull()){
-        nodes_stack_.back()->GetValue() = Array{};
-    } else if (nodes_stack_.back()->IsArray()){
-        cerr << " ADD TO ARRAY ";
-        get<Array>(nodes_stack_.back()->GetValue()).emplace_back(move(Array {}));
-        nodes_stack_.emplace_back(&get<Array>(nodes_stack_.back()->GetValue()).back());
-    } else {
-        throw std::logic_error("error StartArray() : the last opened Node is not Null or Array"s);
-    }
-    //cerr << "\t - end"sv << endl;
-    return *this;
+	if (expects_build_method){
+		throw std::logic_error("Need be Build() after Value()"s);
+	}
+	this->empty_json_builder = false;
+	//cerr << "StartArray() : begin "sv << endl;
+	if (nodes_stack_.empty()){
+		root_ = (Array {});
+		nodes_stack_.emplace_back(&root_);
+	} else if (nodes_stack_.back()->IsNull()){
+		nodes_stack_.back()->GetValue() = Array{};
+	} else if (nodes_stack_.back()->IsArray()){
+		cerr << " ADD TO ARRAY ";
+		get<Array>(nodes_stack_.back()->GetValue()).emplace_back(move(Array {}));
+		nodes_stack_.emplace_back(&get<Array>(nodes_stack_.back()->GetValue()).back());
+	} else {
+		throw std::logic_error("error StartArray() : the last opened Node is not Null or Array"s);
+	}
+	//cerr << "\t - end"sv << endl;
+	return *this;
 }
 
-CommonContext Builder::EndArray(){
-    if (need_be_build){
-        throw std::logic_error("Need be Build() after Value()"s);
-    }
-    //cerr << "EndArray() : begin "sv << endl;
-    if(!(nodes_stack_.back()->IsArray())){
-        throw std::logic_error("error EndArray() : the last opened Node is not json::Array"s);
-    }
-    nodes_stack_.pop_back();
-    //cerr << "\t - end"sv << endl;
-    return *this;
+Builder& Builder::EndArray(){
+	if (expects_build_method){
+		throw std::logic_error("Need be Build() after Value()"s);
+	}
+	//cerr << "EndArray() : begin "sv << endl;
+	if(!(nodes_stack_.back()->IsArray())){
+		throw std::logic_error("error EndArray() : the last opened Node is not json::Array"s);
+	}
+	nodes_stack_.pop_back();
+	//cerr << "\t - end"sv << endl;
+	return *this;
 }
 
 Node Builder::Build(){
-    //cerr << "Build() : begin "sv << endl;
-    if ( root_.IsNull() || this->empty_json_builder || !nodes_stack_.empty()){
-        throw std::logic_error("error Build() : JSON is not fully build"s);
-    }
-    //cerr << "\t - end"sv << endl;
-    return root_;
+	//cerr << "Build() : begin "sv << endl;
+	if ( root_.IsNull() || this->empty_json_builder || !nodes_stack_.empty()){
+		throw std::logic_error("error Build() : JSON is not fully build"s);
+	}
+	//cerr << "\t - end"sv << endl;
+	return root_;
 }
 
 KeyContext BuilderContext::Key(std::string key){
-    return builder_.Key(key);
+	return builder_.Key(move(key));
 }
 
-
 KeyValueContext KeyContext::Value(Node::Value value){
-    builder_.Value(value);
-    return KeyValueContext(builder_);
+	builder_.Value(move(value));
+	return KeyValueContext(builder_);
 }
 
 ArrayContext ArrayContext::Value(Node::Value value){
-    builder_.Value(value);
-    return ArrayContext(builder_);
+	builder_.Value(move(value));
+	return ArrayContext(builder_);
 }
-CommonContext CommonContext::Value(Node::Value value){
-    builder_.Value(value);
-    return CommonContext(builder_);
-}
+
 DictContext BuilderContext::StartDict(){
-    return builder_.StartDict();
+	return builder_.StartDict();
 }
-CommonContext BuilderContext::EndDict(){
-    return builder_.EndDict();
+Builder& BuilderContext::EndDict(){
+	return builder_.EndDict();
 }
 ArrayContext BuilderContext::StartArray(){
-    return builder_.StartArray();
+	return builder_.StartArray();
 }
-CommonContext BuilderContext::EndArray(){
-    return builder_.EndArray();
-}
-Node BuilderContext::Build(){
-    return builder_.Build();
+Builder& BuilderContext::EndArray(){
+	return builder_.EndArray();
 }
 
 } // namespace json
--- transport-catalogue/json_builder.h
+++ transport-catalogue/json_builder.h
@@ -13,23 +13,24 @@ class KeyValueContext;
 class ArrayContext;
 class DictContext;
 class ArrayValueContext;
-class CommonContext;
+//class CommonContext;
 
 
 class Builder{
 public:
-    KeyContext Key(std:: string key);
-    BuilderContext Value(Node::Value value);
-    DictContext StartDict();
-    CommonContext EndDict();
-    ArrayContext StartArray();
-    CommonContext EndArray();
-    Node Build();
+	KeyContext Key(std:: string key);
+	Builder& Value(Node::Value value);
+	DictContext StartDict();
+	Builder& EndDict();
+	ArrayContext StartArray();
+	Builder& EndArray();
+	Node Build();
+
 private:
-    Node root_;
-    std::vector<Node*> nodes_stack_;
-    bool empty_json_builder = true;
-    bool need_be_build = false;
+	Node root_;
+	std::vector<Node*> nodes_stack_;
+	bool empty_json_builder = true;
+	bool expects_build_method = false;
 };
 
 
@@ -37,77 +38,61 @@ private:
 
 class BuilderContext{
 public:
-    BuilderContext(Builder& builder)
-        : builder_(builder){
-    }
+	BuilderContext(Builder& builder)
+		: builder_(builder){
+	}
 
-    KeyContext Key(std::string key);
-    //BuilderContext Value(Node::Value value);
-    DictContext StartDict();
-    CommonContext EndDict();
-    ArrayContext StartArray();
-    CommonContext EndArray();
-    Node Build();
-//private:
-    Builder& builder_;
+	KeyContext Key(std::string key);
+	DictContext StartDict();
+	Builder& EndDict();
+	ArrayContext StartArray();
+	Builder& EndArray();
+
+protected:
+	Builder& builder_;
 };
 
 class KeyContext final : public BuilderContext {
 public:
-    KeyContext(Builder& builder)
-        : BuilderContext(builder){
-    }
-    KeyContext Key(std:: string key) = delete;
-    CommonContext EndDict() = delete;
-    CommonContext EndArray() = delete;
-    Node Build() = delete;
+	KeyContext(Builder& builder)
+		: BuilderContext(builder){
+	}
+	KeyContext Key(std:: string key) = delete;
+	Builder& EndDict() = delete;
+	Builder& EndArray() = delete;
 
-    KeyValueContext Value(Node::Value value);
+	KeyValueContext Value(Node::Value value);
 };
 
 class KeyValueContext final : public BuilderContext {
 public:
-    KeyValueContext(Builder& builder)
-        : BuilderContext(builder){
-    }
-    DictContext StartDict() = delete;
-    ArrayContext StartArray() = delete;
-    CommonContext EndArray() = delete;
-    Node Build() = delete;
+	KeyValueContext(Builder& builder)
+		: BuilderContext(builder){
+	}
+	DictContext StartDict() = delete;
+	ArrayContext StartArray() = delete;
+	Builder& EndArray() = delete;
 };
 
 class DictContext final : public BuilderContext {
 public:
-    DictContext(Builder& builder)
-        : BuilderContext(builder){
-    }
-    //BuilderContext Value(Node::Value value) = delete;
-    DictContext StartDict() = delete;
-    ArrayContext StartArray() = delete;
-    CommonContext EndArray() = delete;
-    Node Build() = delete;
+	DictContext(Builder& builder)
+		: BuilderContext(builder){
+	}
+	DictContext StartDict() = delete;
+	ArrayContext StartArray() = delete;
+	Builder& EndArray() = delete;
 };
 
 class ArrayContext final : public BuilderContext {
 public:
-    ArrayContext(Builder& builder)
-        : BuilderContext(builder){
-    }
-    KeyContext Key(std:: string key) = delete;
-    CommonContext EndDict() = delete;
-    Node Build() = delete;
+	ArrayContext(Builder& builder)
+		: BuilderContext(builder){
+	}
+	KeyContext Key(std:: string key) = delete;
+	Builder& EndDict() = delete;
 
-    ArrayContext Value(Node::Value value);
-
-};
-
-
-class CommonContext final : public BuilderContext {
-public:
-    CommonContext(Builder& builder)
-        : BuilderContext(builder){
-    }
-    CommonContext Value(Node::Value value);
+	ArrayContext Value(Node::Value value);
 };
 
 } // namespace json
